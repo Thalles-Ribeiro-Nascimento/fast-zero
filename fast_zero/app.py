e29fb87096d4b90fc1d1f2152a5f04e5d692f746
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2026-01-29 12:01:14 -0300

    API, Endpoint e Trafegando dados com JSON

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,11 +1,15 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.pydanticHandler import Message
 
 # Inicializando uma aplicação FastAPI - objeto FastAPI()
 app = FastAPI()
 
 
 # Definindo um endpoint com o endereço / acessível pelo método HTTP GET
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def root():
     # Funçao que retorna um dicionário
     return {"message": "Olá, mundo!"}
